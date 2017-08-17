d1a2a17ddd5eb6bbcf1fdc353ce8aea0c7c737f0
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-08-17 02:05:14 -0300

    feat: add structure of the project

---
+++ MyAmazingPod/MyAmazingPod/MyAmazingPod.h
@@ -0,0 +1,19 @@
+//
+//  MyAmazingPod.h
+//  MyAmazingPod
+//
+//  Created by Bruno Macabeus Aquino on 16/08/17.
+//  Copyright © 2017 Bruno Macabeus Aquino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyAmazingPod.
+FOUNDATION_EXPORT double MyAmazingPodVersionNumber;
+
+//! Project version string for MyAmazingPod.
+FOUNDATION_EXPORT const unsigned char MyAmazingPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyAmazingPod/PublicHeader.h>
+
+
